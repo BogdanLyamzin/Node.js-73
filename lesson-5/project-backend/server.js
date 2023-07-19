93cec094cf61494cc22beb3210b999ed2451167b
unknown <bogdan.lyamzin.d@gmail.com>
2023-07-19 21:38:59 +0300

    finish lesson 5

---
+++ lesson-5/project-backend/server.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import app from "./app.js";
+
+dotenv.config();
+
+const {DB_HOST, PORT} = process.env;
+
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`)
+    })
+  })
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+
